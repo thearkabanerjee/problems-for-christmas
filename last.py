0f5e17bddd257f5d8f9c976beeb625f1a99a8c4c
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-26 23:54:49 +0530

    first commit

---
+++ last.py
@@ -0,0 +1,19 @@
+def get_sum(a,b):
+    #good luck!
+    sum = 0
+    if a > b :
+        smallernum = b
+        greaternum = a
+    else:
+        smallernum = a
+        greaternum = b
+
+    if a!=b :
+        for i in range(smallernum, greaternum+1):
+            sum += i
+            
+        return (sum)
+    else:
+        return (a)
+    
+print(get_sum (-1, 2))
